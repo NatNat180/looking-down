05533ae5fa619031d9a15962bb3178bf849c2739
NatNat180 <Nathan.Argall@gmail.com>
2017-10-21 15:08:06 -0500

    move enemy forward upon character detection

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -8,13 +8,12 @@ public class EnemyBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		enemy = GetComponent<Rigidbody>();
-		Debug.Log(enemy.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Physics.Raycast(enemy.transform.position, transform.TransformDirection(Vector3.forward), 10)) {
-			Debug.Log("Character detected!");
+			transform.Translate(Vector3.forward * Time.deltaTime * 3);
 		}
 	}
 }
