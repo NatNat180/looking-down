b7e3c42ba5b9f443ae169548c34615c1f4ede727
NatNat180 <Nathan.Argall@gmail.com>
2017-10-21 11:53:03 -0500

    character tank control movement, enemy detection logged in debug

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+	private Rigidbody enemy;
+
+	// Use this for initialization
+	void Start () {
+		enemy = GetComponent<Rigidbody>();
+		Debug.Log(enemy.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast(enemy.transform.position, transform.TransformDirection(Vector3.forward), 10)) {
+			Debug.Log("Character detected!");
+		}
+	}
+}
---
+++ Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float rotationSpeed;
+	private Rigidbody character;
+
+	// Use this for initialization
+	void Start () {
+		character = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float rotate = Input.GetAxis("Horizontal");
+		float move = Input.GetAxis("Vertical");
+
+		character.velocity = transform.forward * moveSpeed * move;
+		transform.Rotate(Vector3.up * rotationSpeed * rotate * Time.deltaTime);
+
+	}
+}
