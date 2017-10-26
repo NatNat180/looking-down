d5cee73e5fe35bf49030b1d78e06f336554a9a63
NatNat180 <Nathan.Argall@gmail.com>
2017-10-26 15:58:29 -0500

    change name of enemy Rigidbody, change color on RayCast collision, add comments

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
-	private Rigidbody enemy;
+	private Rigidbody enemyBody;
+	private Renderer enemyRenderer;
 
 	// Use this for initialization
 	void Start () {
-		enemy = GetComponent<Rigidbody>();
+		enemyBody = GetComponent<Rigidbody>();
+		enemyRenderer = enemyBody.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Physics.Raycast(enemy.transform.position, transform.TransformDirection(Vector3.forward), 10)) {
-			transform.Translate(Vector3.forward * Time.deltaTime * 3);
+		// if rayCast location @ enemyBody position detects (any) collider
+		if (Physics.Raycast(enemyBody.transform.position, transform.TransformDirection(Vector3.forward), 10)) {
+			// change color of enemyRenderer
+			enemyRenderer.material.color = Color.cyan;
+			// move forward
+			//transform.Translate(Vector3.forward * Time.deltaTime * 3);
+		} else {
+			enemyRenderer.material.color = Color.black;
 		}
 	}
 }
