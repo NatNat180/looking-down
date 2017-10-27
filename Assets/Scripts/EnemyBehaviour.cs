c526f7c01534e433f5e1beac74be9f155ce62bab
Argall, Nathan <Nathan.Argall@gmail.com>
2017-10-27 13:50:14 -0500

    add wall, enemy change color only when Player hits Raycast, reformat code

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour {
-	private Rigidbody enemyBody;
-	private Renderer enemyRenderer;
+public class EnemyBehaviour : MonoBehaviour
+{
+    private Rigidbody enemyBody;
+    private Renderer enemyRenderer;
+    Ray enemyRay;
+	RaycastHit rayHit;
 
-	// Use this for initialization
-	void Start () {
-		enemyBody = GetComponent<Rigidbody>();
-		enemyRenderer = enemyBody.GetComponent<Renderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// if rayCast location @ enemyBody position detects (any) collider
-		if (Physics.Raycast(enemyBody.transform.position, transform.TransformDirection(Vector3.forward), 10)) {
-			// change color of enemyRenderer
-			enemyRenderer.material.color = Color.cyan;
-			// move forward
-			//transform.Translate(Vector3.forward * Time.deltaTime * 3);
-		} else {
-			enemyRenderer.material.color = Color.black;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody>();
+        enemyRenderer = enemyBody.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRay = new Ray(enemyBody.transform.position, transform.TransformDirection(Vector3.forward));
+        /* if rayCast location @ enemyBody position detects player collider */
+        if (Physics.Raycast(enemyRay, out rayHit) && rayHit.collider.tag.Equals("Player"))
+        {
+            /* change color of enemyRenderer */
+            enemyRenderer.material.color = Color.cyan;
+            /* move forward */
+            //transform.Translate(Vector3.forward * Time.deltaTime * 3);
+
+        }
+        else
+        {
+            enemyRenderer.material.color = Color.black;
+        }
+    }
 }
--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -2,25 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCharacterController : MonoBehaviour {
+public class MainCharacterController : MonoBehaviour
+{
 
-	public float moveSpeed;
-	public float rotationSpeed;
-	private Rigidbody character;
+    public float moveSpeed;
+    public float rotationSpeed;
+    private Rigidbody character;
 
-	// Use this for initialization
-	void Start () {
-		character = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		float rotate = Input.GetAxis("Horizontal");
-		float move = Input.GetAxis("Vertical");
+    // Use this for initialization
+    void Start()
+    {
+        character = GetComponent<Rigidbody>();
+    }
 
-		character.velocity = transform.forward * moveSpeed * move;
-		transform.Rotate(Vector3.up * rotationSpeed * rotate * Time.deltaTime);
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+        float rotate = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Vertical");
+
+        character.velocity = transform.forward * moveSpeed * move;
+        transform.Rotate(Vector3.up * rotationSpeed * rotate * Time.deltaTime);
+
+    }
 }
