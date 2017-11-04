251f164c000d1ba2f73533de4f4a70876a419851
Argall, Nathan <Nathan.Argall@gmail.com>
2017-11-04 14:38:43 -0500

    take out unnecessary code for simplification, restructure waypoint loop to avoid constant errors being thrown

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -15,14 +15,12 @@ public class EnemyBehaviour : MonoBehaviour
     Vector3 targetDirection;
     bool isPlayerDetected;
     public Rigidbody playerBody;
-    int playerSpotted;
 
     // Use this for initialization
     void Start()
     {
         enemyBody = GetComponent<Rigidbody>();
         enemyRenderer = enemyBody.GetComponent<Renderer>();
-        playerSpotted = 0;
     }
 
     // Update is called once per frame
@@ -30,40 +28,27 @@ public class EnemyBehaviour : MonoBehaviour
     {
         isPlayerDetected = false;
         enemyRay = new Ray(enemyBody.transform.position, transform.TransformDirection(Vector3.forward));
-        /* if rayCast location @ enemyBody position detects player collider, set to true */
         if (Physics.Raycast(enemyRay, out rayHit) && rayHit.collider.tag.Equals("Player"))
         {
             isPlayerDetected = true;
         }
 
-        /* check that currentWayPoint iteration is less than wayPointList length - that enemy has somewhere to move */
-        if (currentWayPoint < this.wayPointList.Length)
+        if (isPlayerDetected)
         {
-            if (isPlayerDetected)
-            {
-                playerSpotted++;
-                //jump();
-                speed = 5f;
-                enemyRenderer.material.color = Color.red;
-                targetWayPoint = playerBody.transform;
-                targetDirection = playerBody.position - transform.position;
-            }
-            else
-            {
-                playerSpotted = 0;
-                speed = 3f;
-                enemyRenderer.material.color = Color.green;
-                targetWayPoint = wayPointList[currentWayPoint];
-                targetDirection = targetWayPoint.position - transform.position;
-            }
-
-            move();
+            speed = 5f;
+            enemyRenderer.material.color = Color.red;
+            targetWayPoint = playerBody.transform;
+            targetDirection = playerBody.position - transform.position;
         }
         else
         {
-            currentWayPoint = 0;
+            speed = 3f;
+            enemyRenderer.material.color = Color.green;
+            targetWayPoint = wayPointList[currentWayPoint];
+            targetDirection = targetWayPoint.position - transform.position;
         }
 
+        move();
     }
 
     void move()
@@ -79,21 +64,12 @@ public class EnemyBehaviour : MonoBehaviour
         if (transform.position == targetWayPoint.position)
         {
             currentWayPoint++;
+            /* restart waypoint loop when last waypoint is reached */
+            if (currentWayPoint >= wayPointList.Length)
+            {
+                currentWayPoint = 0;
+            }
             targetWayPoint = wayPointList[currentWayPoint];
-        }
-
-    }
-
-    /* jump animation for fun - will be removed later */
-    void jump()
-    {
-        if (playerSpotted <= 1)
-        {
-            transform.Translate(0f, 0.5f, 0f);
-        }
-        else if (playerSpotted == 4)
-        {
-            transform.Translate(0f, -0.5f, 0f);
         }
 
     }
