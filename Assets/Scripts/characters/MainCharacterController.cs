089ea02cc078f7b946fe978d9bab24e75239e47a
Argall, Nathan <Nathan.Argall@gmail.com>
2017-11-27 13:47:16 -0600

    add movement options, take out unneeded crouch function, modify comments, change input manager and project settings

--- Assets/Scripts/characters/MainCharacterController.cs
+++ Assets/Scripts/characters/MainCharacterController.cs
@@ -13,18 +13,21 @@ public class MainCharacterController : MonoBehaviour
     private Collider characterCollider;
     private Vector3 characterNormalScale;
     public GameObject PlayerTorch;
+    private enum MovementOptions { Crouch, Explore, Run };
+    private MovementOptions movementMode;
 
     void Start()
     {
         character = GetComponent<Rigidbody>();
         characterCollider = GetComponent<Collider>();
         characterNormalScale = transform.localScale;
+        movementMode = MovementOptions.Explore;
     }
 
     void Update()
     {
         TorchGrabDetect();
-        Crouch();
+        ChooseMovementMode();
         moveInput = Input.GetAxis("Vertical");
         rotateInput = Input.GetAxis("Horizontal");
     }
@@ -45,14 +48,30 @@ public class MainCharacterController : MonoBehaviour
 
     private void Move()
     {
-        // Create a vector in the direction the character is facing with a magnitude based on the input, speed and time between frames
+        // Default movement and scale
         Vector3 movement = transform.forward * moveInput * moveSpeed * Time.deltaTime;
+        transform.localScale = characterNormalScale;
 
-        // Apply this movement to the rigidbody's position
+        // Determine current movement mode to set movement speed and scale of character
+        if (movementMode == MovementOptions.Run)
+        {
+            // Create a vector in the direction the character is facing with a magnitude based on the input, speed and time between frames
+            movement = transform.forward * moveInput * (moveSpeed * 2) * Time.deltaTime;
+        }
+        else if (movementMode == MovementOptions.Crouch)
+        {
+            // Create a vector in the direction the character is facing with a magnitude based on the input, speed and time between frames
+            movement = transform.forward * moveInput * (moveSpeed / 2) * Time.deltaTime;
+            // Reduce y-axis local scale
+            transform.localScale = characterNormalScale + Vector3.down / 2;
+        }
+
+        // Apply movement to the rigidbody's position
         character.MovePosition(character.position + movement);
+
     }
 
-    private void Rotate() 
+    private void Rotate()
     {
         // Determine the number of degrees to be turned based on the input, speed and time between frames 
         float rotate = rotateInput * rotationSpeed * Time.deltaTime;
@@ -63,14 +82,45 @@ public class MainCharacterController : MonoBehaviour
         // Apply this rotation to the rigidbody's rotation
         character.MoveRotation(character.rotation * turnRotation);
     }
-
-    private void Crouch()
+    
+    private void ChooseMovementMode()
     {
-        if (Input.GetButtonUp("Crouch"))
-            transform.localScale = characterNormalScale;
-        
-        if (Input.GetButton("Crouch"))
-            // Transform local scale of player - decrement half of y-axis
-            transform.localScale = characterNormalScale + Vector3.down / 2;
+        MovementOptions currentMovementMode = movementMode;
+        Debug.Log("Current movement mode = " + currentMovementMode);
+        if (Input.GetButtonDown("ToggleMovementModeRight"))
+        {
+            switch (currentMovementMode)
+            {
+                case MovementOptions.Explore:
+                    movementMode = MovementOptions.Run;
+                    break;
+
+                case MovementOptions.Crouch:
+                    movementMode = MovementOptions.Explore;
+                    break;
+
+                case MovementOptions.Run:
+                    movementMode = MovementOptions.Crouch;
+                    break;
+            }
+        }
+
+        if (Input.GetButtonDown("ToggleMovementModeLeft"))
+        {
+            switch (currentMovementMode)
+            {
+                case MovementOptions.Explore:
+                    movementMode = MovementOptions.Crouch;
+                    break;
+
+                case MovementOptions.Crouch:
+                    movementMode = MovementOptions.Run;
+                    break;
+
+                case MovementOptions.Run:
+                    movementMode = MovementOptions.Explore;
+                    break;
+            }
+        }
     }
 }
