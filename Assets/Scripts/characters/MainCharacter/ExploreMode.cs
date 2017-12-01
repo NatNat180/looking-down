1d778770fbeddcfb5e5ae8796a533842772ba112
Argall, Nathan <Nathan.Argall@gmail.com>
2017-12-01 13:40:29 -0600

    create ExploreMode and RunMode, add to main character

---
+++ Assets/Scripts/characters/MainCharacter/ExploreMode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploreMode : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isExploring())
+        {
+			Jog();
+			Jump();
+			Examine();
+        }
+    }
+
+    private bool isExploring()
+    {
+        return MainCharacterController.movementMode.Equals(MovementOptions.Explore);
+    }
+
+    private void Jog()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Examine()
+    {
+
+    }
+}
---
+++ Assets/Scripts/characters/MainCharacter/RunMode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunMode : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isRunning())
+        {
+			Charge();
+			Leap();
+			Slide();
+        }
+    }
+
+    private bool isRunning()
+    {
+        return MainCharacterController.movementMode.Equals(MovementOptions.Run);
+    }
+
+    private void Charge()
+    {
+
+    }
+
+    private void Leap()
+    {
+
+    }
+
+    private void Slide()
+    {
+
+    }
+}
